71f459b82ed1cf8bec73be64a05c08316eef57cd
2003jpcristhian <cjulp@miumg.edu.gt>
2024-03-03 12:22:47 -0600

    Repositorio Colaborativo

---
+++ CSJP.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+class CSJP{
+	// Atributos para la clase CSJP(private)
+	private : string nombres,apellidos,direccion;
+	
+	void crear();
+	void leer();
+	void actualizar();
+	void borrar();
+};
