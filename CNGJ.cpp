574e36f965df2f62b7498ba1be8c89b46d9f7367
Carlos Gomez <cgomez@yalutec.com>
2024-03-03 12:25:13 -0600

    este es mi cambio

---
+++ CNGJ.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+class CNGJ {
+	private: string nombre, apellidos, direccion;
+	
+	void crear();
+	void leer();
+	void actualizar();
+	void borrar();
+	//ejercicio 
+};
+
