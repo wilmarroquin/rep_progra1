6c17e05d7684ed2821369024cb9f2030d7361ea1
wilmer marroquin <ios8.wil@gmail.com>
2024-03-03 12:23:16 -0600

    clases 1

---
+++ WEMG.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+class WEMG{
+	private: string nombres, apellidos, direccion;
+	
+	void crear();
+	void lee();
+	void actualizar();
+	void borrar();
+	
+};	
